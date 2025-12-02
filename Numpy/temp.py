2ea95301b45905378ac03c216102da95a1fc3cd6
Shahid khan <143776984+Shahid619@users.noreply.github.com>
2025-12-02 08:20:52 +0500

    NumPy array of daily temperatures
    Create a NumPy array of daily temperatures → compute mean.

---
+++ Numpy/temp.py
@@ -0,0 +1,8 @@
+# Create a NumPy array of daily temperatures → compute mean.
+
+import numpy as  np
+Temperature = [12,5,1,8,10,-1,0,0,5,2,1,6,3]
+
+avg_temperature = np.mean(Temperature)
+
+print('average temperature : ',avg_temperature)
